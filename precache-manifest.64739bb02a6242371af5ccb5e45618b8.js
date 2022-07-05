5a9202248bc24ff86e4c47a041c4fabfdba4889e
PrafullGarg_11 <gargprafull55@gmail.com>
2022-07-05 06:20:59 +0530

    Updates

---
+++ precache-manifest.64739bb02a6242371af5ccb5e45618b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a514ba849e84fec5dad",
+    "url": "/texormer/static/css/main.d7576955.chunk.css"
+  },
+  {
+    "revision": "4a514ba849e84fec5dad",
+    "url": "/texormer/static/js/main.4a514ba8.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/texormer/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/texormer/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "27a4bfe26132fa5701a0",
+    "url": "/texormer/static/js/runtime~main.27a4bfe2.js"
+  },
+  {
+    "revision": "64f3b9d248cd3b26881376b2e201a48a",
+    "url": "/texormer/index.html"
+  }
+];
